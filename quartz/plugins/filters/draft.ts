54244b4eeff398cbf45e0db5087f1f68e383de20
namingbe <namingbe@users.noreply.github.com>
2024-10-23 11:29:53 +0200

    post-rebase fixes

--- quartz/plugins/filters/draft.ts
+++ quartz/plugins/filters/draft.ts
@@ -2,7 +2,7 @@ import { QuartzFilterPlugin } from "../types"
 
 export const RemoveDrafts: QuartzFilterPlugin<{}> = () => ({
   name: "RemoveDrafts",
-  shouldPublish(_ctx, [_tree, vfile]) {
+  shouldPublish([_tree, vfile]) {
     const draftFlag: boolean =
       vfile.data?.frontmatter?.draft === true || vfile.data?.frontmatter?.draft === "true"
     return !draftFlag
--- quartz/plugins/filters/explicit.ts
+++ quartz/plugins/filters/explicit.ts
@@ -2,7 +2,7 @@ import { QuartzFilterPlugin } from "../types"
 
 export const ExplicitPublish: QuartzFilterPlugin = () => ({
   name: "ExplicitPublish",
-  shouldPublish(_ctx, [_tree, vfile]) {
+  shouldPublish([_tree, vfile]) {
     return vfile.data?.frontmatter?.publish === true || vfile.data?.frontmatter?.publish === "true"
   },
 })
